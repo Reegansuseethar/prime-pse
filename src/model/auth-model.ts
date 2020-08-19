c225c83be89af7abb56c798c5c7e4a601288ae8e
Reegansuseethar <32837142+Reegansuseethar@users.noreply.github.com>
2020-08-19 17:54:04 +0530

    Update auth-model.ts

--- src/model/auth-model.ts
+++ src/model/auth-model.ts
@@ -1,23 +1,23 @@
-export class AuthModel{
-    private name:string;
-    private permission:string; //All  / none
-    private password:string;
+export class AuthModel {
+    private name: string;
+    private permission: string; //All  / none
+    private password: string;
 
-    constructor(name,permission,password){
+    constructor(name, permission, password) {
         this.name = name;
         this.permission = permission;
         this.password = password;
     }
-private authData = {
-    user1:{
-        "name": "Admin",
-        "permission": "all",
-        "password":"Admin"
-    },
-    user2:{
-        "name": "myName",
-        "permission": "none",
-        "password":"test"
+    private authData = {
+        user1: {
+            "name": "Admin",
+            "permission": "all",
+            "password": "admin@321"
+        },
+        user2: {
+            "name": "myName",
+            "permission": "none",
+            "password": "test"
+        }
     }
-}
 }
